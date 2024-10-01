96c6f373958d9d3a0837594f936441986e12ce14
Владислава <vladoshik8@mail.ru>
2024-10-01 23:53:12 +0300

    Add files via upload

---
+++ task1234.py
@@ -0,0 +1,13 @@
+                            #1st program
+print(9**0.5*5)
+                            #2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+                            #3nd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+                            #4nd program
+name = "123.456"
+print(name[4:5])
+print(float(name))
+print(int(float(name) * 10) % 10)
